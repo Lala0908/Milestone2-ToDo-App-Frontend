7e8f20c5c5d50fafd18e7ed6639c472edf2d5956
Laisla Schultz <laislaschultz@gmail.com>
2023-04-29 16:25:51 -0400

    initial frontend code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+      type="text"
+      value={input}
+      />
+      <button>Add</button>
+      <button>Delete</button>
+
+
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
